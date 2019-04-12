3ced1eb0fd140b4eb49591ede39ee0e8ce3c0d30
Alona <dorozhynskaad@gmail.com>
2019-04-12 14:54:07 +0300

    workig backend

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://Alon:Keksik1995@ds135776.mlab.com:35776/employee-search'
+  mongoURI: 'mongodb://Alon:Keksik1995@ds135776.mlab.com:35776/employee-search',
+  secretOrKey: 'secret'
 };
